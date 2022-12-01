4ff6d9673d806c01577a47263dc91de77a143506
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-01 19:29:19 +0900

    Modify: Router.js 수정

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Login from "../src/pages/Login";
-import Main from "..src/pages/Main";
+import Main from "../src/pages/Main";
 
 export default function Router() {
   return (
