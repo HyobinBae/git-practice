76b248705a86b07f3552536d062c58483d6752b3
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-01 18:41:09 +0900

    Add: 로그인 기능 구현 완료

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
 import Login from "../src/pages/Login";
 
 export default function Router() {
